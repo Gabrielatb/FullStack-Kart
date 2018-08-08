1e89f49ec18c0ab7821cc46e4275daff8a7cf516
Gabriela <tborges.gabriela@gmail.com>
2018-08-08 22:11:13 +0000

    Kart class included in coverage

--- tests.py
+++ tests.py
@@ -18,12 +18,13 @@ def connect_to_db(app):
 
 class Homepage(unittest.TestCase):
         "confirming homepage is as expected"
+
         def setUp(self):
             """Todo before every test"""
 
             self.client = app.test_client()
             app.config['TESTING'] = True
-        # Connect to test database
+            # Connect to test database
             connect_to_db(app)
             db.create_all()
             test_seed_data()
@@ -35,6 +36,13 @@ class Homepage(unittest.TestCase):
             self.assertEqual(result.status_code, 200)
             self.assertIn("<h1 style='color:blue'> Online Shopping made easy! <h1>", result.data)
 
+
+        def redirect(self):
+            """redirects to shop page"""
+            result = self.client.get("/kart")
+            self.assertEqual(result.status_code, 200)
+            self.assertIn("<h1 style='color:blue;text-align:center;margin-top: 5%;'> Add Items to Your Cart </h1>", result.data)
+
         def tearDown(self):
             """Todo after each test."""
 
@@ -44,7 +52,25 @@ class Homepage(unittest.TestCase):
 
 
 
-# class Kart(unittest.TestCase):
+class Kart(unittest.TestCase):
+    def setUp(self):
+    """Todo before every test"""
+
+    self.client = app.test_client()
+    app.config['TESTING'] = True
+    # Connect to test database
+    connect_to_db(app)
+    db.create_all()
+
+
+
+
+    def tearDown(self):
+    """Todo after each test."""
+
+    db.session.close()
+    db.drop_all()
+
 
 
 
